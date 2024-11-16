cd8d8c0223fcf8ad68cc7a61783fae9be68037a5
lucastimho <lucas.t.ho.lh@gmail.com>
2024-11-16 17:44:42 -0600

    changes image retrieval function to accept parameters

--- toyota-analytics-app/src/hooks/useFetch.js
+++ toyota-analytics-app/src/hooks/useFetch.js
@@ -1,15 +1,14 @@
 const axios = require("axios");
 
-const apiKey = "09nftuhkq_qa1dx6n5x_e8ddx6tpv";
-const make = "toyota";
-const model = "tacoma";
-const year = "2018";
-const color = "blue";
-const format = "json";
-
-// Wrap the code in an async function
-
-async function fetchCarImageLink() {
+// Define the function to fetch the car image based on parameters
+async function fetchCarImageLink({
+  apiKey = "09nftuhkq_qa1dx6n5x_e8ddx6tpv",
+  make,
+  model,
+  year,
+  color,
+  format = "json",
+}) {
   try {
     const { data } = await axios.get("https://api.carsxe.com/images", {
       params: {
@@ -21,15 +20,19 @@ async function fetchCarImageLink() {
         format: format,
       },
     });
+
     // Assuming the API returns an object with an array named 'images'
     if (data && data.images && data.images.length > 0) {
-      const firstImageInfo = data.images[0].link;
+      const firstImageInfo = data.images[0];
       console.log(firstImageInfo);
+      return firstImageInfo;
     } else {
       console.log("No images found for the specified parameters.");
+      return null;
     }
   } catch (e) {
     console.error("Error fetching car image:", e);
+    return null;
   }
 }
 

015d7a4fea1ba2d54144bff4f41f06772402a129
lucastimho <lucas.t.ho.lh@gmail.com>
2024-11-16 23:04:59 -0600

    adds openai API

--- python/main.py
+++ python/main.py
@@ -11,16 +11,22 @@ filtered_data = data[data['Mfr Name'] == 'Toyota']
 data = pd.read_csv('2022-FE-Guide-for-DOE-release-dates-before-1-12-2023-no-sales-1-11-2023public.csv')
 filtered_data = filtered_data + data[data['Mfr Name'] == 'Toyota']
 
-# 2023
+# # 2023
 data = pd.read_csv('2023-FE-Guide-for-DOE-final-MY-dataset-no-sales-2-14-2024public.csv')
 filtered_data = filtered_data + data[data['Mfr Name'] == 'Toyota']
 
-# 2024
+# # 2024
 data = pd.read_csv('2024-FE-Guide-for-DOE-release-dates-before-9-17-2024-no-sales-9-17-2024public.csv')
 filtered_data = filtered_data + data[data['Mfr Name'] == 'Toyota']
 
-# 2025
+# # 2025
 data = pd.read_csv('2025-FE-Guide-for-DOE-release-dates-before-10-17-2024-no-sales-10-17-2024public.csv')
 filtered_data = filtered_data + data[data['Mfr Name'] == 'Toyota']
 
-print(filtered_data)
+# filtered_data = data[data['Mfr Name'] == 'Toyota']
+
+X = filtered_data.iloc[:, :4].values
+y = filtered_data.iloc[:, 12:15].values
+
+print(X)
+print(y)
